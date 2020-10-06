bbe96c12b93ca6753bfca02a58a4cf46eb890e59
Wiktor Rzemek <wiktorrzemek@gmail.com>
2020-10-07 01:04:05 +0200

    first commit

---
+++ VoiceChat.Logic/VoiceRoom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace VoiceChat.Logic
+{
+    public class VoiceRoom
+    {
+        public ConcurrentDictionary<string, string> Users { get; set; } = new ConcurrentDictionary<string, string>();
+        public ConcurrentDictionary<string, ChannelWriter<(string, object)>> UsersChannels { get; set; } = new ConcurrentDictionary<string, ChannelWriter<(string, object)>>();
+    }
+}
---
+++ VoiceChat.Web/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VoiceChat.Logic;
+using VoiceChat.Web.Models;
+
+namespace VoiceChat.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly VoiceChatService _voiceChatService;
+        private readonly IMapper _mapper;
+
+        public HomeController(ILogger<HomeController> logger, VoiceChatService voiceChatService, IMapper mapper)
+        {
+            _logger = logger;
+            _voiceChatService = voiceChatService;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateVoiceRoom()
+        {
+            var key = _voiceChatService.CreateVoiceRoom();
+            return RedirectToAction("VoiceRoom", "Home", new { key });
+        }
+
+        public IActionResult VoiceRoom(string key)
+        {
+            if (key == null)
+                return BadRequest();
+
+            var model = _mapper.Map<VoiceRoomModel>(_voiceChatService.GetVoiceRoom(key));
+            if (model == null)
+                return NotFound();
+
+            model.Key = key;
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
